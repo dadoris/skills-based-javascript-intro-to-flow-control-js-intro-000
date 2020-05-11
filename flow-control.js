9025ed94f3554e0e9d8a6941f26e439549eef5f0
dadoris <dadoris@uw.edu>
2020-05-11 05:39:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,20 +1,36 @@
+<<<<<<< HEAD
 function basicTeenager(age) {
   if( age >= 13 && age<=19 ) {
     return "You are a teenager!"
+=======
+var basicTeenager = function(age) {
+  if((age >= 13) && (age <= 19)){
+    return "You are a teenager!" ;
+>>>>>>> 48b89bf14a5e16810b6063ecedc57cd8513e46e6
   }
 }
 
 
 function teenager(age) {
+<<<<<<< HEAD
   if( age >= 13 && age<=19 ) {
     return "You are a teenager!"
   }else{
     return "You are not a teenager"
   }
+=======
+  if(age>=13 && age <=19)
+    return "You are a teenager!";
+  else {
+    return "You are not a teenager";
+    }
+>>>>>>> 48b89bf14a5e16810b6063ecedc57cd8513e46e6
 
+  return false;
 }
 
 function ageChecker(age) {
+<<<<<<< HEAD
   if( age >= 13 && age<=19 ) {
     return "You are a teenager!"
   }else if(age <= 12){
@@ -23,6 +39,16 @@ function ageChecker(age) {
     return "You are a grownup"
   }
 
+=======
+  if(age>=13 && age <=19)
+    return "You are a teenager!"
+  else if(age<13){
+    return "You are a kid"
+  else {
+    return "You are a grownup"
+  }
+return false;
+>>>>>>> 48b89bf14a5e16810b6063ecedc57cd8513e46e6
 }
 
 function ternaryTeenager(age){
